c4d85e7c2fcbc2580bb9678708634e6458b3c4bb
lorandme <149714616+lorandme@users.noreply.github.com>
2024-11-09 16:17:17 +0200

    Implement player.h

--- Proiect_MCPP/Proiect_MCPP/player.h
+++ Proiect_MCPP/Proiect_MCPP/player.h
@@ -1,4 +1,32 @@
 #pragma once
-
+#include <string>
 class Player {
+private:
+    std::string username;
+    int x;
+    int y;
+    int lives;
+    int score;
+
+    void checkCollisionWithEnemies();
+
+public:
+    Player(const std::string& username, int initialX, int initialY);
+
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+
+    void shoot();
+    void loseLife();
+
+    bool isEliminated() const;
+    bool hasWon() const;
+
+    std::pair<int, int> getPosition() const;
+    int getScore() const;
+	int getLives() const;
+
+    void updateScore(int points);
 };

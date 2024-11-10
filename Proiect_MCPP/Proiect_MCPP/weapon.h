f9d4a1a111fb11dc4e5f886296da5cab4ea94304
Andreea Mocanu <andreea-g.mocanu@student.unitbv.ro>
2024-11-10 15:38:19 +0200

    In clasa Weapon se controleaza toate caracteristicile armei

--- Proiect_MCPP/Proiect_MCPP/weapon.h
+++ Proiect_MCPP/Proiect_MCPP/weapon.h
@@ -1,5 +1,26 @@
 #pragma once
 
 class Weapon {
+private:
+	int waitTime;
+	double bulletSpeed;
+	int powerLevel;
+
+public:
+	//Constructor
+	Weapon(int initialWaitTime = 4, double initialSpeed = 0.25);
+
+	//Metode
+
+	bool canShoot() const;
+	void upgradePower();
+	int getWaitTime() const;
+	double getBulletSpeed() const;
+
+	void modifyWaitTime(int newWaitT);
+	void modifyBulletSpeed(double newBulletS);
+
+	//Se va reveni la setarile initiale
+	void reset();
 
 };

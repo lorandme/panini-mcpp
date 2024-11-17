f988880b6a5301f16d7277fd70539cb31f46fe44
Andreea Mocanu <andreea-g.mocanu@student.unitbv.ro>
2024-11-17 19:22:17 +0200

    Functionalitatile clasei Team

--- Proiect_MCPP/Proiect_MCPP/team.h
+++ Proiect_MCPP/Proiect_MCPP/team.h
@@ -1,4 +1,28 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include "Player.h"
+
 class Team {
+private:
+	std::string teamName;
+	std::vector<Player> players;
+	int score;
+	
+public:
+	Team(const std::string& name);
+
+	void addPlayer(const Player& player);
+	void removePlayer(const std::string& playerName);
+	
+	int getScore() const;
+	void updateScore(int points);
+
+	std::string getName() const { return teamName; }
+	std::vector<Player> getPlayers() const { return players; }
+
+	bool hasPlayer(const std::string& playerName) const;
+	Player* findPlayer(const std::string& playerName);
+
 };

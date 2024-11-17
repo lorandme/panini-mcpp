27fab38c7ad009482159a91a293df3cba4e47751
Andreea Mocanu <andreea-g.mocanu@student.unitbv.ro>
2024-11-17 20:03:46 +0200

    Functionalitatile clasei GameManager

--- Proiect_MCPP/Proiect_MCPP/gameManager.h
+++ Proiect_MCPP/Proiect_MCPP/gameManager.h
@@ -1,5 +1,15 @@
 #pragma once
 
 class GameManager {
+private:
+	Map gameMap;
+	std::vector<Player> players;
+	bool isRunning;
 
+public:
+	GameManager();
+	void initializeGame();
+	void startGame();
+	void gameLoop();
+	void endGame();
 };

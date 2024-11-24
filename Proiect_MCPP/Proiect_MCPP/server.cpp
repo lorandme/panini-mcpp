fb66c354d9574ad167cfdfbb836334d7970042ea
isaoprea <isabel.mio47@gmail.com>
2024-11-24 19:58:31 +0200

    server.cpp

---
+++ Proiect_MCPP/Proiect_MCPP/server.cpp
@@ -0,0 +1,32 @@
+﻿#include "crow.h"
+#include <iostream>
+
+int main() {
+    crow::SimpleApp app;
+
+    int scor = 0;
+
+    // Endpoint pentru a primi poziția tancului
+    CROW_ROUTE(app, "/update").methods(crow::HTTPMethod::POST)([&](const crow::request& req) {
+        auto body = crow::json::load(req.body);
+        if (!body) {
+            return crow::response(400, "JSON invalid");
+        }
+        int tancX = body["tancX"].i();
+        int tancY = body["tancY"].i();
+
+        std::cout << "Poziția tancului primită: X=" << tancX << " Y=" << tancY << std::endl;
+
+        // Exemplu: Actualizare scor
+        scor += 10;
+
+        return crow::response(200, "Poziție primită");
+        });
+
+    // Endpoint pentru scor
+    CROW_ROUTE(app, "/score")([&]() {
+        return crow::json::wvalue{ {"scor", scor} };
+        });
+
+    app.port(18080).multithreaded().run();
+}

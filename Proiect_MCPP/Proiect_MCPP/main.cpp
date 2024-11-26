2e897300b62adf23cb5871591fa6663fb54bdc20
lorandme <149714616+lorandme@users.noreply.github.com>
2024-11-26 11:12:45 +0200

    Add SFML-related code

--- Proiect_MCPP/Proiect_MCPP/main.cpp
+++ Proiect_MCPP/Proiect_MCPP/main.cpp
@@ -1,4 +1,5 @@
-﻿#include "GameManager.h"
+﻿#include <SFML/Graphics.hpp>
+#include "GameManager.h"
 #include <iostream>
 
 int main() {
@@ -12,5 +13,23 @@ int main() {
     std::cout << "Jocul a inceput! \n";
     gameManager.startGame();
 
+    // TESTARE SFML
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }

826949cff1c0e7fc8e445675a956356e94114b7f
Taus Radu-Matei <radu.taus@student.unitbv.ro>
2024-12-22 14:57:13 +0200

    Added database.cpp and database.h for SQLite database initialization and user table creation.

---
+++ Proiect_MCPP/Proiect_MCPP/database.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "sqlite3.h"
+#include "database.h"
+
+void initializeDatabase() {
+    sqlite3* db;
+    char* errMessage = 0;
+
+    // Deschide sau creează baza de date
+    const char* db_path = "C:/Users/matei/Documents/Proiect_MC/Proiect_MCPP/Proiect_MCPP/battle_city.db";
+    int rc = sqlite3_open(db_path, &db);
+    if (rc) {
+        std::cerr << "Nu s-a putut deschide baza de date: " << sqlite3_errmsg(db) << std::endl;
+        return;
+    }
+
+    // Creează tabela Users dacă nu există deja
+    const char* sql = R"(
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL
+        );
+    )";
+
+    rc = sqlite3_exec(db, sql, 0, 0, &errMessage);
+    if (rc != SQLITE_OK) {
+        std::cerr << "Eroare la crearea tabelei Users: " << errMessage << std::endl;
+        sqlite3_free(errMessage);
+    }
+    else {
+        std::cout << "Tabela 'Users' a fost creata sau exista deja.\n";
+    }
+
+    sqlite3_close(db);
+}
---
+++ Proiect_MCPP/Proiect_MCPP/database.h
@@ -0,0 +1,6 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+void initializeDatabase();
+
+#endif // DATABASE_H

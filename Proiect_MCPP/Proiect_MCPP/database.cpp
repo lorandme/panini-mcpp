a22d438639277a574501ef5224e7048e752f8e9b
isaoprea <isabel.mio47@gmail.com>
2024-12-26 19:04:57 +0200

    Modificare cpp database pentru functionalitate universala

--- Proiect_MCPP/Proiect_MCPP/database.cpp
+++ Proiect_MCPP/Proiect_MCPP/database.cpp
@@ -1,20 +1,33 @@
 ﻿#include <iostream>
-#include "sqlite3.h"
+#include <sqlite3.h>
 #include "database.h"
+#include <fstream>
+#include <direct.h>
+#include <Windows.h>
+
+
+//Un path pentru database sa functioneze pentru toata lumea
+std::string getDatabasePath() {
+   
+    char buffer[MAX_PATH];
+    _getcwd(buffer, MAX_PATH); 
+    std::string dbPath = std::string(buffer) + "\\battle_city.db";  
+    return dbPath;
+}
 
 void initializeDatabase() {
     sqlite3* db;
     char* errMessage = 0;
 
-    // Deschide sau creează baza de date
-    const char* db_path = "C:/Users/matei/Documents/Proiect_MC/Proiect_MCPP/Proiect_MCPP/battle_city.db";
-    int rc = sqlite3_open(db_path, &db);
+    
+    std::string db_path = getDatabasePath();
+
+    int rc = sqlite3_open(db_path.c_str(), &db);
     if (rc) {
         std::cerr << "Nu s-a putut deschide baza de date: " << sqlite3_errmsg(db) << std::endl;
         return;
     }
 
-    // Creează tabela Users dacă nu există deja
     const char* sql = R"(
         CREATE TABLE IF NOT EXISTS Users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,

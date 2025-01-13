69c18a8ebfa44efe7d9c17b8096505ace360de7c
lorandme <149714616+lorandme@users.noreply.github.com>
2025-01-13 15:27:04 +0200

    Implemented LoginScreen Interface

---
+++ Proiect_MCPP/Frontend/LoginScreen.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class LoginScreen {
+public:
+    explicit LoginScreen(sf::RenderWindow& window);
+    void run();
+
+private:
+    sf::RenderWindow& window;
+
+    sf::Font font;
+    sf::Text usernameLabel;
+    sf::Text passwordLabel;
+    sf::RectangleShape usernameBox;
+    sf::RectangleShape passwordBox;
+    sf::RectangleShape loginButton;
+    sf::Text loginButtonText;
+
+    std::string usernameInput;
+    std::string passwordInput;
+    bool isUsernameActive = false;
+    bool isPasswordActive = false;
+
+    void processEvents();
+    void render();
+    void handleInput(sf::Event& event);
+    void drawTextBox(sf::RenderWindow& target, sf::RectangleShape& box, const std::string& text);
+};

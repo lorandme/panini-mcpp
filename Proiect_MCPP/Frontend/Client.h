46dd3d9db8aa16a9fcdaed1eb1fce1176fccfab4
isaoprea <isabel.mio47@gmail.com>
2025-01-17 02:02:30 +0200

    Eliminare comentarii inutile

--- Proiect_MCPP/Frontend/Client.h
+++ Proiect_MCPP/Frontend/Client.h
@@ -1,32 +1,4 @@
-﻿//#pragma once
-//
-//#include <winsock2.h>
-//#include <ws2tcpip.h>
-//#include <string>
-//#include <nlohmann/json.hpp>
-//
-//class Client {
-//private:
-//    SOCKET m_socket;
-//    std::string m_serverIP;
-//    int m_serverPort;
-//
-//public:
-//    Client(const std::string& serverIP, int serverPort);
-//
-//    ~Client();
-//
-//    void init();
-//
-//    // Functie pentru conversia unui sir wide char in UTF-8
-//    std::string wstringToUtf8(const std::wstring& wstr);
-//
-//    // Trimiterea unui mesaj catre server
-//    void sendMessage(const nlohmann::json& request);
-//
-//    // Primirea unui raspuns de la server
-//    nlohmann::json receiveMessage();
-//};
+﻿
 #ifndef CLIENT_H
 #define CLIENT_H
 
@@ -34,19 +6,14 @@
 
 class Client {
 public:
-    // Constructor
     Client(const std::string& serverUrl);
-
-    // Funcție pentru înregistrare utilizator
     bool registerUser(const std::string& username, const std::string& password);
-
-    // Funcție pentru autentificare utilizator
     bool login(const std::string& username, const std::string& password);
 
 private:
-    std::string serverUrl;  // URL-ul serverului la care se face cererea
+    std::string serverUrl; 
 };
 
-#endif // CLIENT_H
+#endif 
 
 

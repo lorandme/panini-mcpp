7066bf1c627bbbdf9266840439805550d7e7b680
lorandme <149714616+lorandme@users.noreply.github.com>
2025-01-19 00:21:27 +0200

    Update Bullet

--- Proiect_MCPP/Proiect_MCPP/Bullet.cpp
+++ Proiect_MCPP/Proiect_MCPP/Bullet.cpp
@@ -1,8 +1,15 @@
 #include "Bullet.h"
+#include <iostream>
 
-Bullet::Bullet(double startX, double startY, Direction dir, double speed)
-    : m_x(startX), m_y(startY), m_direction(dir), m_speed(speed), m_isActive(true) {}
+Bullet::Bullet(double startX, double startY, Direction dir, int ownerId)
+    : m_x(startX), m_y(startY),
+    m_direction(dir),
+    m_speed(BASE_SPEED),
+    m_isActive(true),
+    m_ownerId(ownerId) {
+}
 
+// In Bullet.cpp
 void Bullet::move(double deltaTime) {
     if (m_isActive) {
         switch (m_direction) {
@@ -27,8 +34,12 @@ void Bullet::deactivate() {
 }
 
 bool Bullet::checkCollision(int targetX, int targetY) const {
-    return m_x == targetX && m_y == targetY;
+    const double epsilon = 0.7;
+    double dx = std::abs(m_x - targetX);
+    double dy = std::abs(m_y - targetY);
+    return dx < epsilon && dy < epsilon;
 }
+
 
 std::pair<double, double> Bullet::getPosition() const {
     return { m_x, m_y };
@@ -39,10 +50,10 @@ bool Bullet::isBulletActive() const {
 }
 
 void Bullet::setPosition(double newX, double newY) {
-    m_x = newX;  
-    m_y = newY;  
+    m_x = newX;
+    m_y = newY;
 }
 
 void Bullet::setDirection(Direction newDirection) {
-    m_direction = newDirection;  
+    m_direction = newDirection;
 }
